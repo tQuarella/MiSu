49576cc57c9485c73b29586b7aac709a6fa6b169
Tony Quarella <tquarella@gmail.com>
2013-03-09 17:12:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiSu::Application.config.secret_token = 'a048dc37b336a45fa49abe37cbf0a374fa2c5ecb7148e75375e0bd6c9fd885067b64a44839c315a5b74771adbfa33fc8f2ffd0955525ad3915fcb42eb68e4459'
